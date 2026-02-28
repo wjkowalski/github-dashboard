830208db43f1d4a724a36db551c0079d6e84b669
Bill <williamkowalski@gmail.com>
2026-02-28 09:25:52 -0400

    updoots

--- practice.js
+++
@@ -1,8 +0,0 @@
-
-let url = 'https://api.github.com/search/repositories?q=stars:%3E1&sort=stars&order=desc&per_page=100';
-
-fetch(url)
-.then( response => response.json())
-.then( data => {
-    console.log(data);
-})
--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 
-(function(){
+(function () {
 
     let body = document.querySelector('body');
     let readout = document.createElement('div');
@@ -8,62 +8,146 @@
 
     let url = 'https://api.github.com/search/repositories?q=stars:%3E1&sort=stars&order=desc&per_page=100';
 
- try  { fetch(url)
-    .then( response => response.json())
-    .then( data => {
-        console.log("Fetching Github data...");
-
-        let repos = data.items;
-
-        for(const repo of repos){
-            let name = repo.full_name;
-            let desc = repo.description;
-            let forks = repo.forks_count;
-            let topics = repo.topics;
-            let watchers = repo.watchers;
-            let clone_url = repo.clone_url;
-            let homepage = repo.homepage;
-            let github_link = repo.html_url;
-            let avatar = repo.owner.avatar_url;
-
-            const output = document.createElement('div');
-            body.appendChild(output);
-
-            let image = "<img src='" + avatar + "'>";
-            let imageHolder = document.createElement('div');
-            imageHolder.setAttribute('class', 'imageHolder');
-            output.appendChild(imageHolder);
-            image.innerHTML = image;
+    // function for the Clipboard API
 
 
+    try {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                console.log("Fetching Github data...");
 
-            output.innerHTML = "<h2 style='margin-bottom: 0'>" + name +  "</h2>" +  desc;
+                // Get our list of repos from the main array
+                let repos = data.items;
 
-            let topicsHolder = document.createElement('div');
-            topicsHolder.setAttribute('class', 'topicHolder');
-            output.appendChild(
-                topicsHolder
-            );
-            topicsHolder.style.flexWrap = "wrap";
-            
-            for(j = 0; j < topics.length; j++){
-                let topic = document.createElement('div');
-                topic.innerHTML = topics[j];
-                topic.style.backgroundColor = "black";
-                topic.style.color = "white";
-                topic.style.padding = ".25rem";
-                topic.style.borderRadius = "5px";
-                topicsHolder.append(topic);
-            }
+                console.log(repos);
 
-            topicsHolder.style.display = "flex";
-            topicsHolder.style.gap = ".5rem";
-            output.style.marginBottom = "1rem";
-        }
+                for (const repo of repos) {
+                    let name = repo.full_name;
+                    let desc = repo.description;
+                    let forks = repo.forks_count;
+                    let topics = repo.topics;
+                    let watchers = repo.watchers;
+                    let clone_url = repo.clone_url;
+                    let homepage = repo.homepage;
+                    let github_link = repo.html_url;
+                    let avatar = repo.owner.avatar_url;
+                    let stars = repo.stargazers_count;
 
-    })
-} catch{
-    console.log("There was an error.", error);
-}
+                    // make a container for each repo listing
+                    const output = document.createElement('div');
+                    output.class = 'output';
+
+
+                    body.append(output);
+
+                    // make a container for the image
+                    let imHolder = document.createElement('div');
+                    imHolder.class = imHolder;
+                    imHolder.style.maxWidth = "30%";
+                    output.append(imHolder);
+
+                    // make the image
+                    let image = "<img style='height: 200px; width=200px; border-radius: 25px' src='" + avatar + "'>";
+
+                    // put the image in the image container
+                    imHolder.innerHTML = image;
+
+                    // get the name and description 
+                    let nameDesc = "<h2 style='margin-bottom: 0'>" + name + "</h2>" + desc + "<br /><h3>Stars: " + stars + "</h3>";
+
+                    // build a container to hold name and description
+                    let ndHolder = document.createElement('div');
+                    ndHolder.class = 'ndHolder';
+
+
+                    // put name and desc in the container
+                    ndHolder.innerHTML = nameDesc;
+
+                    // build a top section in output that will hold image and name/desc
+                    let top = document.createElement('div');
+                    top.setAttribute('class', 'top');
+
+                    // put this section at the top of output
+                    output.append(top);
+
+                    // put the pic/name/desc in the top section
+                    top.append(imHolder, ndHolder);
+                    top.classList.add('top');
+
+                    // create container for links to repo, website, and copy clone URL if they exist
+                    let linksHolder = document.createElement('div');
+                    linksHolder.setAttribute('class', 'linksHolder');
+                    output.appendChild(linksHolder);
+
+                    // create buttons for each link if there is anything there
+                    if (github_link) {
+                        console.log("Found a github link.");
+                        let btnGh = document.createElement('a');
+                        btnGh.class = 'button';
+                        btnGh.id = 'button-gh';
+                        btnGh.href = github_link;
+                        btnGh.target = '_blank';
+                        btnGh.rel = 'noopener noreferrer';
+                        btnGh.innerHTML = "REPO" + "<span class='sr-only'>(opens in new tab)</span>";
+                        linksHolder.append(btnGh);
+                    }
+
+                    if (clone_url) {
+                        console.log("Found the clone url.");
+                        let btnClone = document.createElement('a');
+                        btnClone.setAttribute('class', 'button');
+                        btnClone.setAttribute('id', 'btn-clone');
+                        linksHolder.append(btnClone);
+                        btnClone.innerHTML = "CLONE URL";
+                        btnClone.addEventListener('click', () => {
+                            alert("The clone URL has been copied to your clipboard.")
+                            navigator.clipboard.writeText(clone_url)
+                        });
+                    }
+
+                    if (homepage) {
+                        console.log("Found a homepage link.");
+                        let btnHome = document.createElement('a');
+                        btnHome.setAttribute('class', 'button');
+                        btnHome.innerHTML = "WEBSITE" + "<span class='sr-only'>(opens in new tab)</span>";
+                        btnHome.target = "_blank";
+                        btnHome.rel = 'noopener noreferrer'; // security layer that prevents tabnabbing via window.opener, and also prevents the destination site from seeing where the user came from
+                        btnHome.href = homepage;
+                        linksHolder.append(btnHome);
+                    }
+
+
+                    // build a container for the topics
+                    let topicsHolder = document.createElement('div');
+                    topicsHolder.setAttribute('class', 'topicHolder');
+                    topicsHolder.style.flexWrap = "wrap";
+
+                    // add the topics holder to output
+                    output.appendChild(
+                        topicsHolder
+                    );
+
+                    // Topics
+                    for (j = 0; j < topics.length; j++) {
+                        let topic = document.createElement('div');
+                        topic.innerHTML = topics[j];
+                        topic.style.backgroundColor = "black";
+                        topic.style.color = "white";
+                        topic.style.padding = ".25rem";
+                        topic.style.borderRadius = "5px";
+                        topicsHolder.append(topic);
+                    }
+                    topicsHolder.style.display = "flex";
+                    topicsHolder.style.gap = ".5rem";
+                }
+                // All done, bye bye
+                console.log("Fetching complete.");
+            })
+
+
+    } catch {
+        console.log("There was an error.", error);
+    }
 
 })();
